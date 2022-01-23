e04ca14557e57d7f72f6e7194f4569e7778f6f18
Muhammad Trad <muhammad.trad@yahoo.com>
2022-01-22 22:21:19 -0800

    feat: challenge 1 - public key balances

--- server/index.js
+++ server/index.js
@@ -2,24 +2,43 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+const key = ec.genKeyPair();
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+// const balances = {
+//   "1": 100,
+//   "2": 50,
+//   "3": 75,
+// }
+
+const balances = {};
+const keyPair = {};
+
+
+const numberOfAddresses = 10;
+
+const generateKey = () => {
+  const key = ec.genKeyPair();
+  const publicKey = key.getPublic().encode('hex');
+  const shortPublicKey = '0x' + publicKey.toString('hex').slice(0, -70);
+  balances[shortPublicKey] = 100;
+  keyPair[shortPublicKey] = '0x' + key.getPrivate().toString(16);
 }
 
+// get the balance of an address 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
   const balance = balances[address] || 0;
   res.send({ balance });
 });
-
+// send an amount from the sender to the recipient
 app.post('/send', (req, res) => {
   const {sender, recipient, amount} = req.body;
   balances[sender] -= amount;
@@ -28,5 +47,15 @@ app.post('/send', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
+  console.log(`Listening on port ${port}!\n`);
+  console.log('Available Accounts');
+  console.log('==================');
+
+  for (let i=0; i<numberOfAddresses; i++) {
+    generateKey();
+  }
+  Object.keys(balances).forEach((address, index) => console.log(`(${index}) ${address} (${balances[address]} ETH)`));
+  console.log('\nPrivate Keys');
+  console.log('============');
+  Object.keys(keyPair).forEach((address, index) => console.log(`(${index}) ${keyPair[address]} `));
 });
